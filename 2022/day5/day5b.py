8843915e5cd0142965dcc2581e86c2e8dc11255c
Mark Carlson <carlsonmark@gmail.com>
2022-12-05 13:53:31 -0700

    AOC 2022 Day 5

---
+++ 2022/day5/day5b.py
@@ -0,0 +1,78 @@
+# Same as before, but crates stay in same order when moving
+import dataclasses
+
+test_input_a = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+"""
+
+test_input_b = """move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+
+
+def parse_stacks(s):
+    # When saving in pycharm, the space at the end of the line is removed,
+    # so check the last line for the number of stacks
+    last_line = next(reversed(s.splitlines(keepends=False)))
+    stacks = [[] for _ in range(len(last_line) // 4 + 1)]
+    # Iterate lines in reverse order so bottom is at the start
+    for line in reversed(s.splitlines(keepends=False)):
+        for i in range(len(line) // 4 + 1):
+            crate = line[i*4+1]
+            if crate != ' ':
+                stacks[i].append(crate)
+    return stacks
+
+
+@dataclasses.dataclass
+class Move:
+    how_many: int
+    from_where: int
+    to_where: int
+
+
+def parse_moves(s):
+    moves = []
+    for line in s.splitlines(keepends=False):
+        line = line.replace('move ', '').replace('from ', '').replace('to ', '')
+        split = line.split(' ')
+        moves.append(Move(how_many=int(split[0]),
+                          from_where=int(split[1]) - 1,
+                          to_where=int(split[2]) - 1))
+    return moves
+
+
+def execute_move(stacks, move):
+    from_stack = stacks[move.from_where]
+    to_stack = stacks[move.to_where]
+    removed = from_stack[-move.how_many:]
+    stacks[move.from_where] = from_stack[:-move.how_many]
+    to_stack.extend(removed)
+    return stacks
+
+
+def execute_moves(stacks, moves):
+    for move in moves:
+        stacks = execute_move(stacks, move)
+    return stacks
+
+
+stacks = parse_stacks(test_input_a)
+moves = parse_moves(test_input_b)
+stacks = execute_moves(stacks, moves)
+solution = ''
+for stack in stacks:
+    solution += stack[-1]
+print(f'{solution=}')
+
+
+stacks = parse_stacks(open('day5-input-a.txt').read())
+moves = parse_moves(open('day5-input-b.txt').read())
+stacks = execute_moves(stacks, moves)
+solution = ''
+for stack in stacks:
+    solution += stack[-1]
+print(f'{solution=}')
